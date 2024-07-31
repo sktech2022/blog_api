2889a01879f78e6e003a59b44d468dbc151d028e
sreekanth <pasreekanth15@gmail.com>
2024-07-31 22:17:23 +0530

    commiting

--- blog_pro/settings.py
+++ blog_pro/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0(7eqw570hi-mxm()y+=z@gw9f+tjiaopg$v+f3@l#vnidt)lc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["51.20.141.105"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
